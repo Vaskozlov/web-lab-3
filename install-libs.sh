8da34402cdc40f4740a78e162422066bf23ea688
vaskozlov <vaskozlov3940@gmail.com>
2025-04-12 12:04:18 +0300

    feat!: ant

---
+++ install-libs.sh
@@ -0,0 +1,10 @@
+mkdir lib
+curl https://repo1.maven.org/maven2/org/glassfish/jakarta.faces/4.1.1/jakarta.faces-4.1.1.jar > lib/jakarta.faces-4.1.1.jar
+curl https://repo1.maven.org/maven2/org/jetbrains/annotations/26.0.2/annotations-26.0.2.jar > lib/annotations-26.0.2.jar
+curl https://repo1.maven.org/maven2/org/glassfish/jakarta.faces/4.1.1/jakarta.faces-4.1.1.jar > lib/jakarta.faces-4.1.1.jar
+curl https://repo1.maven.org/maven2/jakarta/faces/jakarta.faces-api/4.1.1/jakarta.faces-api-4.1.1.jar > lib/jakarta.faces-api-4.1.1.jar
+curl https://repo1.maven.org/maven2/org/postgresql/postgresql/42.7.4/postgresql-42.7.4.jar > lib/postgresql-42.7.4.jar
+curl https://repo1.maven.org/maven2/jakarta/platform/jakarta.jakartaee-api/10.0.0/jakarta.jakartaee-api-10.0.0.jar > lib/jakarta.jakartaee-api-10.0.0.jar
+curl https://repo1.maven.org/maven2/org/primefaces/primefaces/14.0.6/primefaces-14.0.6-jakarta.jar > lib/primefaces-14.0.6-jakarta.jar
+curl https://repo1.maven.org/maven2/org/jetbrains/kotlin/kotlin-stdlib/1.9.25/kotlin-stdlib-1.9.25.jar > lib/kotlin-stdlib-1.9.25.jar
+curl https://repo1.maven.org/maven2/org/jetbrains/kotlinx/kotlinx-coroutines-core/1.9.0/kotlinx-coroutines-core-1.9.0.jar > lib/kotlinx-coroutines-core-1.9.0.jar
